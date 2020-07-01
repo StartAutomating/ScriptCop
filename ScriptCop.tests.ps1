5e27c8445ee3e3099fb4847879140f026fc2163f
StartAutomating <james.brundage@start-automating.com>
2020-06-30 19:45:24 -0700

    Adding Pester Tests (which call ScriptCop tests)

---
+++ ScriptCop.tests.ps1
@@ -0,0 +1,25 @@
+﻿#requires -Module Pester, ScriptCop
+
+describe ScriptCop {
+    context 'Static Analysis' {
+        it 'Can enforce rules' {
+            function foo {
+
+            }
+
+            Get-command foo |
+                Test-Command -Rule Test-CommandNamingConvention |
+                Select-Object -ExpandProperty Problem |
+                should -BeLike *StandardVerb-CustomNoun*
+        }
+    }
+
+    context 'Testing' {
+        it 'Can reload and test itself' {
+            $theModule = Get-Module ScriptCop
+            Import-Module ($theModule |
+                Split-Path) -Global -Force -PassThru |
+                   Test-Module
+        }
+    }
+}
