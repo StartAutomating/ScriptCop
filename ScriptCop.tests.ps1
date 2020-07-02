f5da706cc067df5390523572236af30ce34f874e
StartAutomating <james.brundage@start-automating.com>
2020-07-01 19:30:54 -0700

    Adding tests for Repair-Command.

--- ScriptCop.tests.ps1
+++ ScriptCop.tests.ps1
@@ -14,26 +14,64 @@ describe ScriptCop {
         }
     }
 
+    context 'Auto-Repair' {
+        it 'Will automatically fix some problems' {
+            $tempModulePsd1 = @'
+@{
+    ModuleVersion = '0.1'
+    Description = 'A Module'
+}
+'@
+
+            $tempModuleName = "TempModule$(Get-Random)"
+
+            $tempModuleRoot =
+                if ($PSVersionTable.OS -and $PSVersionTable.OS -notlike '*Windows*') {
+                    Join-Path '/tmp' $tempModuleName
+                } else {
+                    Join-Path $env:Temp $tempModuleName
+                }
+
+            $tempModulePath = Join-Path $tempModuleRoot "$tempModuleName.psd1"
+            $null = New-item -ItemType File -Force -Path $tempModulePath
+            $tempModulePsd1 | Set-Content $tempModulePath
+
+            $importedModule = Import-Module $tempModulePath -Force -PassThru
+            $importedModule|
+                Test-Command |
+                Repair-Command -WarningAction SilentlyContinue
+
+            $newText = [IO.File]::ReadAllText($tempModulePath) #  | should -Not -Be $tempModulePsd1
+            if ($newText -eq $tempModulePath) {
+                throw "Expected Repair-Command to change things.  $tempModulePath was unchanged."
+            }
+            $importedModule | Remove-Module
+            Remove-Item $tempModulePath -Recurse -Force
+        }
+    }
+
+
     context 'Dynamic Registration' {
         it 'Can remove a registered rule' {
             Unregister-ScriptCopRule -Name Test-DocumentationQuality
             $(Get-ScriptCopRule |
-                Where-Object Name -EQ Test-DocumentationQuality) | should be $null
+                Where-Object Name -EQ Test-DocumentationQuality) | should -Be $null
         }
 
         it 'Can remove a registered fixer' {
             Unregister-ScriptCopFixer -Name Repair-ModuleManifest
             $(Get-ScriptCopFixer |
-                Where-Object Name -EQ Repair-ModuleManifest) | should be $null
+                Where-Object Name -EQ Repair-ModuleManifest) | should -Be $null
 
         }
 
         it 'Can remove a registered patrol' {
             Unregister-ScriptCopPatrol -Name Test-Documentation
             $(Get-ScriptCopPatrol |
-                Where-Object Name -EQ Test-Documentation) | should be $null
+                Where-Object Name -EQ Test-Documentation) | should -Be $null
         }
     }
+
 
     context 'Self-Testing' {
         it 'Can reload and test itself' {
@@ -44,4 +82,5 @@ describe ScriptCop {
                    Test-Module -GetCommandCoverage
         }
     }
+
 }
